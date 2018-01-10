af99d54f95e905c3771482dfa7f55ba246a9505f
Peter Macaldowie <2258785m@student.gla.ac.uk>
2018-01-10 12:44:27 +0000

    At chapter 5.7

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+#Import database models
+from rango.models import Category, Page
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Page)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    #for unicode support of Python 2.x
+    def __unicode__(self):
+        return self.name
+
+    #Set plural for display
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    #for unicode support of Python 2.x
+    def __unicode__(self):
+        return self.title
